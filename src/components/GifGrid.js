cafc1e74499f344fb89b51ac865033d713055157
Nicolas Rios <dnicolas.rios98@gmail.com>
2021-10-03 14:20:18 +0200

    Add custom hook useFetchGifs

--- src/components/GifGrid.js
+++ src/components/GifGrid.js
@@ -1,32 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 import { GifGridItem } from './GifGridItem';
 
 export const GifGrid = ({ category }) => {
-	const [gifs, setGifs] = useState([]);
-
-	useEffect(() => {
-		getGifs();
-	}, []);
-
-	const getGifs = async () => {
-		const url =
-			'https://api.giphy.com/v1/gifs/search?q=age+of+empires&limit=10&api_key=nKqbfWjdlPnOusoIhA9I5Fbs0q4YBvID';
-		const resp = await fetch(url);
-		const { data } = await resp.json();
-
-		const gifs = data.map((gif) => {
-			return {
-				id: gif.id,
-				title: gif.title,
-				url: gif.images?.downsized_medium.url,
-			};
-		});
-		setGifs(gifs);
-	};
+	const { data: gifs, loading } = useFetchGifs(category);
 
 	return (
 		<div>
 			<h3>{category}</h3>
+			{loading && <p>Loading...</p>}
 			<ol className="card-container">
 				{gifs.map((gif) => (
 					<GifGridItem key={gif.id} {...gif} />
